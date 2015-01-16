c04861f106c56e178e65ed4ef1208a3ed51c2777
Mark McDowall <markus.mcd5@gmail.com>
2015-01-15 16:30:09 -0800

    Command queue
    New: Adding multiple series will queue them instead of running all at once
    New: Slower scheduled tasks won't be block others from running

--- src/UI/Commands/CommandModel.js
+++ src/UI/Commands/CommandModel.js
@@ -6,6 +6,13 @@ module.exports = Backbone.Model.extend({
 
     parse : function(response) {
         response.name = response.name.toLocaleLowerCase();
+
+                for (var key in response.body) {
+                    response[key] = response.body[key];
+                }
+
+                delete response.body;
+
         return response;
     },
 
